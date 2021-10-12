2a8b26db218b3fe2d930a7a4f3273629384949d0
Sahil Mulani <40736800+SahillMulani@users.noreply.github.com>
2021-10-12 23:06:39 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include<iostream>
+using namespace std;
+
+int main(int arg, char* args[])
+{
+
+	cout<<"YAY"<<endl;
+	cin.get();
+
+	return 0;
+
+}
